fc28a1a691a4ed12b61b1d85e279956299361b94
yaseralsalam <840704330@moe.bh>
2019-06-17 12:10:20 +0300

    view question and options

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { QuestionProvider } from '../providers/question/question';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { QuestionProvider } from '../providers/question/question';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,87 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ToastController } from 'ionic-angular';
+import { QuestionProvider } from '../../providers/question/question';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  questionsData:any;
+  questionCount:number = 10;
+  questionIndex=0;
+  rgValue;
+  choices=[];
+  constructor(public navCtrl: NavController, private questionProvider:QuestionProvider, private toastCtrl:ToastController) {
+this.loadQuestions();
   }
 
+  loadQuestions()
+  {
+    this.questionProvider.getQuestions().subscribe(
+      data=>{
+        
+      this.questionsData = data;
+      console.log(this.questionsData);
+      console.log(this.questionsData.results[0].question);
+      this.questionsData.results[0].question = this.replaceChar(this.questionsData.results[0].question);
+      console.log("question replaced");
+      this.getChoices();
+      console.log(Math.floor(Math.random()*4)+1);
+      console.log(this.questionsData.results.length);
+      if(this.questionsData.results.length==0)
+      {
+        
+        let toast = this.toastCtrl.create(
+          {
+            message: "No data have been found.",
+            duration: 3000,
+            position: "middle"
+          }
+        );
+        toast.present();
+        toast.onDidDismiss(()=>
+        {}
+        );
+      }
+
+    },
+    (err)=>{
+      let toast = this.toastCtrl.create(
+        {
+          message: "An error occurred while loading data. Please check your connection.",
+          duration: 3000,
+          position: "middle"
+        }
+      );
+      toast.present();
+      toast.onDidDismiss(()=>
+      {}
+      );
+    });
+  }
+
+  replaceChar(str:string)
+  {
+    while(str.indexOf("&#039;")!=-1)
+     str = str.replace("&#039;","'");
+    while(str.indexOf("&quot;")!=-1) 
+     str = str.replace("&quot;","\"");
+    
+   return str;
+  }
+
+  getChoices()
+{
+  for(let i =0;i<3;i++)
+  {
+    this.questionsData.results[this.questionIndex].incorrect_answers[i] = this.replaceChar(this.questionsData.results[this.questionIndex].incorrect_answers[i]);
+    this.choices[i] = this.questionsData.results[this.questionIndex].incorrect_answers[i]
+    
+   }
+  this.questionsData.results[this.questionIndex].correct_answer= this.replaceChar(this.questionsData.results[this.questionIndex].correct_answer);
+  this.choices[3] = this.questionsData.results[this.questionIndex].correct_answer;
+  console.log(this.choices);
+  
+}
 }
